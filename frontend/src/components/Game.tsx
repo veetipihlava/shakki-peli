ddb8e3cac1149e977ce155b10c8e9d7083964f81
Veeti Pihlava <veeti.pihlava@gmail.com>
2025-04-04 10:55:30 +0300

    resolved merge

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -1,5 +1,7 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+import { Chessboard } from "react-chessboard";
+import { Chess, Move } from "chess.js";
 
 const Game: React.FC = () => {
     const playerID: Number = Number(sessionStorage.getItem("playerID"));
@@ -12,7 +14,6 @@ const Game: React.FC = () => {
         reconnectAttempts: 10,
         reconnectInterval: 3000,
     });
-    const [userMessage, setUserMessage] = useState('');
 
     useEffect(() => {
         if (readyState === ReadyState.OPEN) {
@@ -25,50 +26,82 @@ const Game: React.FC = () => {
 
             const json = JSON.stringify(joinMessage);
             console.log("Sending: ", json);
-
             sendMessage(json);
         }
     }, [readyState]);
 
     useEffect(() => {
-        if (lastMessage !== null) {
-            setMessageHistory((prev) => prev.concat(lastMessage));
+        if (lastMessage == null) {
+            return;
         }
+
+        const messageData = JSON.parse(lastMessage.data);
+        setMessageHistory((prev) => prev.concat(messageData));
+
+        if (messageData.type === 'join') {
+            if (messageData.player_id === playerID) {
+                const color = messageData.content === "true" ? "white" : "black";
+                setColor(color);
+            }
+        } else if (messageData.type === 'move') {
+            const serverMove = messageData.content;
+            const move = makeAMove(serverMove);
+            if (!move) {
+                console.log("Invalid move from server: ", serverMove);
+            }
+        } 
     }, [lastMessage]);
 
-    const handleClickSendMessage = useCallback((event: React.FormEvent) => {
-        event.preventDefault();
+    const [color, setColor] = useState<"white" | "black">("white");
+    const [game, setGame] = useState(new Chess());
+
+    function makeAMove(move: { from: string; to: string; promotion?: string }): Move | null {
+        const gameCopy = new Chess(game.fen());
+        const result = gameCopy.move(move);
+
+        if (result) {
+            setGame(gameCopy);
+        }
+
+        return result;
+    }
+
+    function isDraggablePiece({ piece }: { piece: string }): boolean {
+        return piece.charAt(0) === color.charAt(0);
+    }
+
+    function onDrop(sourceSquare: string, targetSquare: string): boolean {
+        const move = makeAMove({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q",
+        });
+
+        if (!move) {
+            return false;
+        }
+
         const messageObject = {
             type: 'move',
             game_id: gameID,
             player_id: playerID,
-            content: userMessage
-          };
+            content: convertToServerMove(move),
+        };
 
         const json = JSON.stringify(messageObject)
         console.log("Sending: ", json);
 
         sendMessage(json);
-        setUserMessage('');
-    }, [userMessage]);
-    
+
+        return true;
+    }
+
     return (
         <div>
-            <h1>Chess game</h1>
             <div>Game ID: {String(gameID)}</div>
-            <form onSubmit={handleClickSendMessage}>
-                <input
-                    type="text"
-                    value={userMessage}
-                    onChange={(e) => setUserMessage(e.target.value)}
-                    placeholder="Type a message..."
-                />
-                <button type="submit" disabled={readyState !== ReadyState.OPEN}>
-                    Send
-                </button>
-            </form>
+            <Chessboard position={game.fen()} onPieceDrop={onDrop} isDraggablePiece={isDraggablePiece} boardOrientation={color} />
             {lastMessage ? <div>Last message: {lastMessage.data}</div> : null}
-            <h2>Message history</h2>
+            <h2>debug message history</h2>
             <ul>
                 {messageHistory.map((message, idx) => (
                     <li key={idx}>{message ? message.data : null}</li>
@@ -77,5 +110,13 @@ const Game: React.FC = () => {
         </div>
     );
 };
+
+function convertToServerMove(move: Move): string {
+    let notation: string = move.piece.charAt(0)
+    notation += move.before
+    notation += move.after
+    console.log(notation.toLowerCase())
+    return notation.toLowerCase();
+}
 
 export default Game;
